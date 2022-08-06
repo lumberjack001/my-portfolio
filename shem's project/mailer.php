fa425d4a4caac9c74913a663329f55b21f6423a7
shem itoya <87392726+lumberjack001@users.noreply.github.com>
2022-08-06 20:57:20 +0100

    changed email
    used current email

--- shem's project/mailer.php
+++ shem's project/mailer.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "rachet.clank001@gmail.com";
+        $recipient = "itoya.shem2017@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
